64d652de445220aa3f9e9d247723dd48126998f7
Jeremiah <jeremiah@switcherstudio.com>
2019-08-13 23:23:53 -0400

    Add repeating mode to timer class

--- timer.py
+++ timer.py
@@ -25,6 +25,7 @@ class Timer2:
 		self.started_at = 0
 		self.duration = duration
 		self.is_active = False
+		self.is_repeating = False
 
 
 	def set_duration(self, duration):
@@ -36,17 +37,28 @@ class Timer2:
 		self.started_at = pygame.time.get_ticks()
 
 
+	def start_repeating(self):
+		self.is_repeating = True
+		self.start()
+
+
 	def reset(self):
 		self.is_active = False
+		self.is_repeating = False
 		self.started_at = 0
 
 
 	def is_finished(self):
+		if self.is_active == False:
+			return False
 		now = pygame.time.get_ticks()
-		timer_expired = now - self.started_at >= self.duration
+		timer_expired = now - self.started_at > self.duration
 		is_finished = True if timer_expired else False
 		
 		if is_finished:
-			self.reset()
+			if self.is_repeating:
+				self.start_repeating()
+			else: 
+				self.reset()
 
 		return is_finished
