1bd1806fb35ce4933962d486ab40a88198085614
jer bushau <exjerlock@hotmail.com>
2019-08-19 18:16:54 -0400

    Changed name firing_timer -> cooldown_timer; Move ammo to weapon class; Move bullet_list to weapon class; Add fire method

--- components/weapon.py
+++ components/weapon.py
@@ -5,15 +5,27 @@ class Weapon:
 
     def __init__(self, ammo_type):
         self.is_firing = False
-        self.firing_timer = Timer(180)
+        self.cooldown_timer = Timer(180)
         self.ammo_type = ammo_type
+        self.ammo = 1000
+        self.bullets = pygame.sprite.Group()
 
 
-    def begin_fire(self):
+    def begin_fire(self, pos):
         self.is_firing = True
-        self.firing_timer.start_repeating()
+        self.fire(pos, True)
+        self.cooldown_timer.start_repeating()
 
+    def fire(self, pos, first_shot=False):
+        if (first_shot
+        or (self.cooldown_timer.is_finished()
+        and self.ammo > 0)):
+            self.ammo_type(pos, self.bullets)
+            self.ammo -= 1
 
     def cease_fire(self):
         self.is_firing = False
-        self.firing_timer.reset()
+        self.cooldown_timer.reset()
+
+    def bullet_collisions(self, **targets):
+        pass
